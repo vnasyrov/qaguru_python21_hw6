d09485ec19763d8ca115798544a0dfd2645293bf
Valeriy Nasyrov <valerius.nasyrov@yandex.ru>
2025-09-13 17:31:52 +0300

    Первый commit

--- homework.py
+++ homework.py
@@ -7,8 +7,10 @@ def test_dark_theme_by_time():
     """
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
-
-    is_dark_theme = None
+    if current_time >= time(22) or current_time <= time(6):
+        is_dark_theme = True
+    else:
+        is_dark_theme = False
     assert is_dark_theme is True
 
 
